86d2b120e1f9ae5a0737b4783ed405001c709482
PTMcEneaney <pagemceneaney@gmail.com>
2019-04-09 12:57:17 -0700

    after converting to an object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,186 +1,182 @@
-//adding all the universial variables
 
-var guesses, wins, gamePlaying, hiddenWord, currentWord, guessesRemaining, dashDisplay, randomNumber;
+var wordGuessGame = {
+    guesses: undefined,
+    wins: undefined,
+    gamePlaying: undefined,
+    hiddenWord: undefined,
+    currentWord: undefined,
+    guessesRemaining: undefined,
+    dashDisplay: undefined,
+    randomNumber: undefined,
 
+    nextWord: function nextWord() { //arrow function doesn't have a value for the word this --> will reference outside the arrow
+        this.guesses = [];
+        this.guessesRemaining = 15;
+        this.hiddenWord = [];
+        this.gamePlaying = true;
+        document.getElementById('hiddenWord').textContent = this.dashDisplay;
+        document.getElementById('guesses').textContent = "Hit any letter to start!";
+        document.getElementById('guessesRemaining').textContent = this.guessesRemaining;
+    
+        document.getElementById("nextWord").classList.add("disabled");
+        this.randomNumber = Math.floor(Math.random() * this.words.length);
+        this.dashGenerator(this.words[this.randomNumber]);
 
+        console.log('working');
+    },
 
-function nextWord() {
-    guesses = [];
-    guessesRemaining = 15;
-    hiddenWord = [];
-    gamePlaying = true;
-    document.getElementById('hiddenWord').textContent = dashDisplay;
-    document.getElementById('guesses').textContent = "Hit a key to start!";
-    document.getElementById('guessesRemaining').textContent = guessesRemaining;
+    //"Press any key to get started" changes the state variable and resets the var values
+    init: function init() {
+        this.nextWord();
+        this.wins = 0;
+        document.getElementById('wins').textContent = this.wins;
 
-    document.getElementById("nextWord").classList.add("disabled");
-    randomNumber = Math.floor(Math.random() * words.length);
-    dashGenerator(words[randomNumber]);
-}
+        //uncomment below if I want the video removed when the reset button ins pressed
+        // document.getElementById('starterPic').classList.remove("d-none");
+        // document.getElementById('video').classList.add("d-none");
 
-//"Press any key to get started" changes the state variable and resets the var values
-function init() {
-    nextWord();
-    wins = 0;
-    document.getElementById('wins').textContent = wins;
-}
+    },
 
-function dashGenerator(song) {
-    for (i = 0; i < song.length; i++) {
-        if (song.charAt(i) == " ") {
-            hiddenWord.push(" ");
-        } else {
-            hiddenWord.push("_");
+    dashGenerator: function dashGenerator(song) {
+        for (i = 0; i < song.length; i++) {
+            if (song.charAt(i) == " ") {
+                this.hiddenWord.push(" ");
+            } else {
+                this.hiddenWord.push("_");
+            }
         }
-    }
-    dashDisplay = hiddenWord.join('');
-    document.getElementById('hiddenWord').textContent = dashDisplay;
-    console.log(song);
-    currentWord = song;
+        this.dashDisplay = this.hiddenWord.join('');
+        document.getElementById('hiddenWord').textContent = this.dashDisplay;
+        console.log(song);
+        this.currentWord = song;
+    },
+
+    words: [
+        'space oddity', //david bowie
+        'ziggy stardust', //david bowie
+        'life on mars', //david bowie
+        'starman', //david bowie
+        'moonage daydream', // david bowie
+        'rocket man', //elton john
+        'intergalactic', //beastie boys
+        'walking on the sun', // smash mouth
+        'girl from mars', //ash
+        'we are all made of stars', //moby
+        'space song', //beach house
+        'spaceman', //the killers
+        'champange supernova', //oasis
+        'man on the moon', //REM
+        'bad moon rising', //CCR
+        'dancing in the moonlight', //king harvest
+        'sisters of the moon', //fleetwood mac
+        'harvest moon', //neil young
+        'major tom', //peter schilling
+        'across the universe', //the beatles
+        'parallel universe', //red hot chili peppers
+        'space jam', //quad city DJ's
+        'staring at the sun', //tv on the radio
+        'black hole sun', //soundgarden
+    ],
+
+    youTube: [
+        'https://www.youtube.com/embed/iYYRH4apXDo?rel=0;&autoplay=1', // 'space oddity', david bowie
+        'https://www.youtube.com/embed/XXq5VvYAI1Q?rel=0;&autoplay=1', // 'ziggy stardust', david bowie
+        'https://www.youtube.com/embed/AZKcl4-tcuo?rel=0;&autoplay=1', // 'life on mars', david bowie
+        'https://www.youtube.com/embed/tRcPA7Fzebw?rel=0;&autoplay=1', // 'starman', david bowie
+        'https://www.youtube.com/embed/JFDj3shXvco?rel=0;&autoplay=1', // 'moonage daydream', david bowie
+        'https://www.youtube.com/embed/DtVBCG6ThDk?rel=0;&autoplay=1', // 'rocket man', elton john
+        'https://www.youtube.com/embed/qORYO0atB6g?rel=0;&autoplay=1', // 'intergalactic', beastie boys
+        'https://www.youtube.com/embed/LQj--Kjn0z8?rel=0;&autoplay=1', // 'walking on the sun', smash mouth
+        'https://www.youtube.com/embed/FkSl9GGOFHM?rel=0;&autoplay=1', // 'girl from mars', ash
+        'https://www.youtube.com/embed/x1rFAaAKpVc?rel=0;&autoplay=1', // 'we are all made of stars', moby
+        'https://www.youtube.com/embed/f9X1C7pTu-M?rel=0;&autoplay=1', // 'space song', beach house
+        'https://www.youtube.com/embed/Hc16Y9fiCvQ?rel=0;&autoplay=1', // 'spaceman', the killers
+        'https://www.youtube.com/embed/LR4kY8LGx7M?rel=0;&autoplay=1', // 'champange supernova', oasis
+        'https://www.youtube.com/embed/dLxpNiF0YKs?rel=0;&autoplay=1', // 'man on the moon', REM
+        'https://www.youtube.com/embed/5BmEGm-mraE?rel=0;&autoplay=1', // 'bad moon rising', CCR
+        'https://www.youtube.com/embed/sR47TZdJg64?rel=0;&autoplay=1', // 'dancing in the moonlight', king harvest
+        'https://www.youtube.com/embed/t76vsuJHe1Q?rel=0;&autoplay=1', //'sisters of the moon', fleetwood mac
+        'https://www.youtube.com/embed/n2MtEsrcTTs?rel=0;&autoplay=1', // 'harvest moon', neil young
+        'https://www.youtube.com/embed/OMDbX1zksgI?rel=0;&autoplay=1', // 'major tom', peter schilllilng
+        'https://www.youtube.com/embed/90M60PzmxEE?rel=0;&autoplay=1', // 'across the universe', beatles
+        'https://www.youtube.com/embed/VIfxDoQDaFE?rel=0;&autoplay=1', // 'parallel universe', red hot chili peppers
+        'https://www.youtube.com/embed/-gUd88gc8WA?rel=0;&autoplay=1', //  'space jam', quad city DJ's
+        'https://www.youtube.com/embed/oHrTOQ18yzU?rel=0;&autoplay=1', // 'staring at the sun', tv on the radio
+        'https://www.youtube.com/embed/3mbBbFH9fAg?rel=0;&autoplay=1', // 'black hole sun', soundgarden
+    
+    ],
+
+    eventKey: function(event) {
+        console.log(this.gamePlaying);
+        //prevents browser from applying default key press behavior
+        //determine if key is a letter or not (regular expressions/keycode values)
+        if (event.key.toUpperCase() === event.key.toLowerCase() || event.key.length > 1) {
+            //if upper case and lower case are the same then it's not a letter
+            return;
+        } 
+        if (this.gamePlaying) {
+            this.guesses.push(event.key);
+            this.guessesRemaining--;
+    
+            var isGuessCorrect = false;
+            //********************** */
+            for (i = 0; i < this.guesses.length; i++) {
+                if (event.key == this.guesses[i] && i < this.guesses.length - 1) {
+                    alert('You already guessed that!')
+                    this.guessesRemaining++;
+                    this.guesses.pop();
+                    return;    
+                    //IF THERE IS A DUPE, ADD A GUESS BACK, REMOVE THE LAST ITEM IN THE ARARY, AND ADJUST THE GUESSES DISPLAY
+                    //(CHECKS EVERY INDEX EXCEPT THE LAST ONE, THIS IS BECASUE THE EVENT.KEY GETS PUSHED TO THE END, SO WE DON'T WANT THE EVENT.KEY TO CHECK AGAINST IT SELF AND GET A FALSE POSITIVE.)           
+                }
+            }
+            
+            //********************** PUSHING GUESSES TO THE CURRENT WORD BOX */
+            for (i = 0; i < this.hiddenWord.length; i++) {
+                if (event.key == this.currentWord.charAt(i)) {
+    
+                    this.hiddenWord[i] = event.key;
+                    this.dashDisplay = this.hiddenWord.join('');
+                    document.getElementById('hiddenWord').textContent = this.dashDisplay;
+                    isGuessCorrect = true;
+                }
+            }
+
+            if (isGuessCorrect === true) {
+                this.guessesRemaining++;
+            }
+    
+            document.getElementById('guesses').textContent = this.guesses;
+            document.getElementById('guessesRemaining').textContent = this.guessesRemaining;
+    
+            //********************* WIN CONDITIONS */
+            if (this.currentWord === this.dashDisplay) {
+                this.wins++;
+                document.getElementById('wins').textContent = this.wins;
+                document.getElementById('guessesRemaining').textContent = "You Did It!";
+                document.getElementById("nextWord").classList.remove("disabled");
+                document.getElementById('starterPic').classList.add("d-none");
+                document.getElementById('video').classList.remove("d-none");
+    
+                var insertVideo = this.youTube[this.randomNumber];
+                document.getElementById('video').src = insertVideo;
+    
+                this.gamePlaying = false;
+            } 
+    
+                //*********************OUT OF GUESSES */
+            if (this.guessesRemaining < 1) {
+                alert("You Lose!")
+                this.init()
+            }
+        }
+    },
 }
 
-var words = [
-    'space oddity', //david bowie
-    'ziggy stardust', //david bowie
-    'life on mars', //david bowie
-    'starman', //david bowie
-    'moonage daydream', // david bowie
-    'rocket man', //elton john
-    'intergalactic', //beastie boys
-    'walking on the sun', // smash mouth
-    'girl from mars', //ash
-    'we are all made of stars', //moby
-    'space song', //beach house
-    'spaceman', //the killers
-    'supersonic rocket ship', //the kinks
-    'champange supernova', //oasis
-    'man on the moon', //REM
-    'bad moon rising', //CCR
-    'dancing in the moonlight', //king harvest
-    'sisters of the moon', //fleetwood mac
-    'harvest moon', //neil young
-    'major tom', //peter schilling
-    'across the universe', //the beatles
-    'parallel universe', //red hot chili peppers
-    'space jam', //quad city DJ's
-    'staring at the sun', //tv on the radio
-    'black hole sun', //soundgarden
-];
-
-var youTube = [
-    'https://www.youtube.com/embed/iYYRH4apXDo?rel=0;&autoplay=1', //space oddity
-    'https://www.youtube.com/embed/XXq5VvYAI1Q?rel=0;&autoplay=1', //ziggy stardust
-    'https://www.youtube.com/embed/AZKcl4-tcuo?rel=0;&autoplay=1', //life on mars
-    'https://www.youtube.com/embed/tRcPA7Fzebw?rel=0;&autoplay=1', //starman
-    'https://www.youtube.com/embed/JFDj3shXvco?rel=0;&autoplay=1', //moonage daydream
-    'https://www.youtube.com/embed/DtVBCG6ThDk?rel=0;&autoplay=1', //rocketman
-    'https://www.youtube.com/embed/qORYO0atB6g?rel=0;&autoplay=1', //intergalactic
-    'https://www.youtube.com/embed/LQj--Kjn0z8?rel=0;&autoplay=1', //walking on the sun
-    'https://www.youtube.com/embed/FkSl9GGOFHM?rel=0;&autoplay=1', //girl from mars
-    'https://www.youtube.com/embed/x1rFAaAKpVc?rel=0;&autoplay=1', //we are all made of stars
-    'https://www.youtube.com/embed/f9X1C7pTu-M?rel=0;&autoplay=1', //space song
-    'https://www.youtube.com/embed/Hc16Y9fiCvQ?rel=0;&autoplay=1', //spaceman
-    'https://www.youtube.com/embed/LR4kY8LGx7M?rel=0;&autoplay=1', //'supersonic rocket ship', the kinks
-    'https://www.youtube.com/embed/LR4kY8LGx7M?rel=0;&autoplay=1', // 'champange supernova', oasis
-    'https://www.youtube.com/embed/dLxpNiF0YKs?rel=0;&autoplay=1', //REM
-    'https://www.youtube.com/embed/5BmEGm-mraE?rel=0;&autoplay=1', //CCR
-    'https://www.youtube.com/embed/sR47TZdJg64?rel=0;&autoplay=1', //king harvest
-    'https://www.youtube.com/embed/t76vsuJHe1Q?rel=0;&autoplay=1', //fleetwood mac
-    'https://www.youtube.com/embed/n2MtEsrcTTs?rel=0;&autoplay=1', //neil young
-    'https://www.youtube.com/embed/OMDbX1zksgI?rel=0;&autoplay=1', //peter schilllilng
-    'https://www.youtube.com/embed/90M60PzmxEE?rel=0;&autoplay=1', //beatles
-    'https://www.youtube.com/embed/VIfxDoQDaFE?rel=0;&autoplay=1', //red hot chili peppers
-    'https://www.youtube.com/embed/-gUd88gc8WA?rel=0;&autoplay=1', //quad city DJ's
-    'https://www.youtube.com/embed/oHrTOQ18yzU?rel=0;&autoplay=1', //tv on the radio
-    'https://www.youtube.com/embed/3mbBbFH9fAg?rel=0;&autoplay=1', //soundgarden
-
-
-
-];
-
-
-
+wordGuessGame.init();
 
 //MOST OF THE FUNCTIONALITY IS TRIGGERED BY A KEYPRESS
-document.onkeypress = function(event) {
-    if (gamePlaying) {
-        guesses.push(event.key);
-        guessesRemaining--;
-
-        //********************** */
-        for (i = 0; i < guesses.length; i++) {
-            if (guesses.length <= 1) {
-                document.getElementById('guesses').textContent = guesses;
-                document.getElementById('guessesRemaining').textContent = guessesRemaining;
-                //**IF THERE IS ONLY ONE GUESS, IT DOESN'T CHECK FOR DUPE LETTERS */
-            } else if (guesses.length >= 2 && event.key !== guesses[i]) {
-                document.getElementById('guesses').textContent = guesses;
-                document.getElementById('guessesRemaining').textContent = guessesRemaining;
-                //** IF THERE ARE 2 OR MORE GUESSES, IT CHECKS EACH INDEX IN THE ARRAY FOR A MATCH TO PREVENT DUPE GUESSES */
-            } else if (event.key == guesses[i] && i < guesses.length - 1) {
-                alert('You already guessed that!')
-                guessesRemaining++;
-                guesses.pop();
-                document.getElementById('guesses').textContent = guesses;
-                return guessesRemaining;     
-                //IF THERE IS A DUPE, ADD A GUESS BACK, REMOVE THE LAST ITEM IN THE ARARY, AND ADJUST THE GUESSES DISPLAY
-                //(CHECKS EVERY INDEX EXCEPT THE LAST ONE, THIS IS BECASUE THE EVENT.KEY GETS PUSHED TO THE END, SO WE DON'T WANT THE EVENT.KEY TO CHECK AGAINST IT SELF AND GET A FALSE POSITIVE.)           
-            }
-        }
-        //********************** PUSHING K GUESSES TO THE CURRENT WORD BOX */
-        for (i = 0; i < hiddenWord.length; i++) {
-            if (event.key == currentWord.charAt(i)) {
-                hiddenWord[i] = event.key;
-                dashDisplay = hiddenWord.join('');
-                document.getElementById('hiddenWord').textContent = dashDisplay;
-            }
-        }
-        //********************* WIN CONDITIONS */
-        if (currentWord === dashDisplay) {
-            wins++;
-            document.getElementById('wins').textContent = wins;
-            document.getElementById('guessesRemaining').textContent = "You Did It!";
-            document.getElementById("nextWord").classList.remove("disabled");
-            document.getElementById("nextWord").classList.remove("disabled");
-            document.getElementById('starterPic').classList.add("d-none");
-            document.getElementById('video').classList.remove("d-none");
-
-            var insertVideo = youTube[randomNumber];
-            document.getElementById('video').src = insertVideo;
-
-
-            gamePlaying = false;
-        } 
-
-
-
-
-
-        //*********************OUT OF GUESSES */
-        if (guessesRemaining < 1) {
-            alert("You Lose!")
-            init()
-        }
-    }
-}
-
-init();
-document.getElementById('nextWord').addEventListener('click', nextWord); 
-document.getElementById('resetGame').addEventListener('click', init); 
-
-
-//TODO LIST
-//Fix event listener so that it records key strokes properly
-//check if event.key matches any hidden-word elements
-//ask tutor if there is a way to remove commas from a printed array. If I can do that, I may not have to create the var dashDisplay and join the array on line 56
-//if I can't remove commas, figure out how to replace letters, probably using .charAt()
-//hide element of dash-arary or change that array item to the event.key
-
-//add win conditions and update score
-//add win banner
-//create next-word function that resets guesses and calls the dashGenerator function
-//add functionality to the reset game button that calls the init();
-//check that all scores etc are reset in the init();
-
-
-//|| event.key == guesses[i-2] || event.key == guesses[i-3] || event.key == guesses[i-4] || event.key == guesses[i-5] || event.key == guesses[i-6] || event.key == guesses[i-7] || )
+document.onkeydown = wordGuessGame.eventKey.bind(wordGuessGame);
+document.getElementById('nextWord').addEventListener('click', wordGuessGame.nextWord.bind(wordGuessGame)); 
+document.getElementById('resetGame').addEventListener('click', wordGuessGame.init.bind(wordGuessGame)); 
+//had to bind so that this. was referencing wordGuessGame not the document
